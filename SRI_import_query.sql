e4b21e38f71ef0db229dce6ab65a41306d70271f
andrew martin <almartin@gmail.com>
2011-08-22 11:17:35 -0400

    change class name logic SRI

--- SRI_import_query.sql
+++ SRI_import_query.sql
@@ -9,7 +9,7 @@ select SIS_ID
       ,lower(first_clean || '.' || last_clean) as USER_NAME
       ,lower(first_clean) as PASSWORD
       ,case when SIS_ID > 0 then 'Rise Academy' else null end as SCHOOL_NAME
-      ,case when SIS_ID > 0 then 2019 else null end as CLASS_NAME
+      ,2024 + (-1 * GRADE) as CLASS_NAME
 from
 (select s.student_number as SIS_ID
       ,s.grade_level as GRADE
@@ -20,5 +20,5 @@ from
       ,s.first_name as PASSWORD
 from students s
 left outer join reenrollments re on s.id = re.studentid
-where s.enroll_status < 1 and re.entrydate is null and s.schoolid = 73252)
+where s.enroll_status < 1 and re.entrydate is null and s.schoolid = 133570965)
 order by GRADE, LAST_NAME, FIRST_NAME
