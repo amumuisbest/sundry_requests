e8b2d59835964d997750a0b2f381942c07b22338
andrew martin <almartin@gmail.com>
2012-04-18 14:47:47 -0400

    which 5th graders made it to 8th or higher?

---
+++ 5th_to_8th_cohort_progression.sql
@@ -0,0 +1,71 @@
+SELECT decode(grouping(level_1.abbrev),0,level_1.abbrev,'total') yr_abbrev
+      ,count(*) as N
+      ,ROUND((AVG(level_1.graduate_flag)*100),0) pct_thru_8th
+FROM
+      (SELECT basis.studentid
+            ,basis.lastfirst
+            ,basis.grade_level as base_gr
+            ,basis.abbreviation as abbrev
+            ,basis.year
+            ,eighth_outcome.grade_level as eighth_gr
+            ,basis.cohort as start_cohort
+            ,basis.highest_achieved as base_high_ach
+            ,eighth_outcome.exitcode
+            ,CASE
+              --edge case: 8th grade transfers
+               WHEN basis.highest_achieved = 8 AND eighth_outcome.exitcode LIKE 'T%' THEN 0
+              --8th grade graduate or better 
+               WHEN basis.highest_achieved >= 8 THEN 1
+               ELSE 0
+             END as graduate_flag
+      FROM cohort_table_long basis
+      LEFT OUTER JOIN cohort_table_long eighth_outcome
+        ON basis.studentid = eighth_outcome.studentid
+       AND eighth_outcome.grade_level = 8
+      WHERE basis.grade_level = 5
+        AND basis.year <= 2008
+      ORDER BY start_cohort
+              ,lastfirst
+      ) level_1
+GROUP BY ROLLUP(level_1.abbrev)
+ORDER BY level_1.abbrev
+;
+
+--verbose version of above query: listagg students to see WHO transferred
+SELECT decode(grouping(level_1.abbrev),0,level_1.abbrev,'total') yr_abbrev
+      ,count(*) as N
+      ,ROUND((AVG(level_1.graduate_flag)*100),0) pct_thru_8th
+      ,listagg(level_1.transfers, ' | ') within group (order by level_1.transfers) as transfers
+FROM
+      (SELECT basis.studentid
+            ,basis.lastfirst
+            ,basis.grade_level as base_gr
+            ,basis.abbreviation as abbrev
+            ,basis.year
+            ,eighth_outcome.grade_level as eighth_gr
+            ,basis.cohort as start_cohort
+            ,basis.highest_achieved as base_high_ach
+            ,eighth_outcome.exitcode
+            ,CASE
+              --edge case: 8th grade transfers
+               WHEN basis.highest_achieved = 8 AND eighth_outcome.exitcode LIKE 'T%' THEN 0
+              --8th grade graduate or better 
+               WHEN basis.highest_achieved >= 8 THEN 1
+               ELSE 0
+             END as graduate_flag
+            ,CASE
+               WHEN basis.highest_achieved = 8 AND eighth_outcome.exitcode LIKE 'T%' THEN basis.lastfirst
+               WHEN basis.highest_achieved < 8 THEN basis.lastfirst
+               ELSE null
+             END as transfers
+      FROM cohort_table_long basis
+      LEFT OUTER JOIN cohort_table_long eighth_outcome
+        ON basis.studentid = eighth_outcome.studentid
+       AND eighth_outcome.grade_level = 8
+      WHERE basis.grade_level = 5
+        AND basis.year <= 2008
+      ORDER BY start_cohort
+              ,lastfirst
+      ) level_1
+GROUP BY level_1.abbrev
+ORDER BY level_1.abbrev
